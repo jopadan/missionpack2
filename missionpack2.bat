6cf6b13e2082f80ee24a0d005f2e4f1b8bdfe057
Dmitri <dmitri.poterianski@gmail.com>
2025-06-22 06:06:47 -0400

    Update windows launcher bat file
    Now checks for and attempts to run with engines in this order:
    
    1) ioquake3 64 bits (ioquake3.x86_64.exe)
    2) ioquake3 32bits (ioquake3.x86.exe)
    3) vanilla (quake3.exe)

--- missionpack2.bat
+++ missionpack2.bat
@@ -1,4 +1,12 @@
 @echo off
 cd %~dp0
 cd ..
-start ioquake3.x86_64.exe +set fs_game missionpack2
+IF EXIST ioquake3.x86_64.exe (
+	start ioquake3.x86_64.exe +set fs_game missionpack2
+) ELSE (
+	IF EXIST ioquake3.x86.exe (
+		start ioquake3.x86.exe +set fs_game missionpack2
+	) ELSE (
+		start quake3.exe +set fs_game missionpack2
+	)
+)
